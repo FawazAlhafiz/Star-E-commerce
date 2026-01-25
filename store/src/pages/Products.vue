d26288914769395e972232efd269e18613e6226d
FawazALhafiz <fawaztwix111@gmail.com>
2026-01-25 22:47:28 +0200

    feat: add Products page and update routing for product listing

---
+++ store/src/pages/Products.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1 class="text-2xl font-bold text-gray-800">
+   ✨ Products 👌
+  </h1>
+</template>
+
+<script setup>
+
+</script>
--- store/src/router.js
+++ store/src/router.js
@@ -1,38 +1,44 @@
-import { userResource } from "@/data/user"
-import { createRouter, createWebHistory } from "vue-router"
-import { session } from "./data/session"
+import { userResource } from "@/data/user";
+import { createRouter, createWebHistory } from "vue-router";
+import { session } from "./data/session";
 
 const routes = [
 	{
 		path: "/",
 		name: "Home",
 		component: () => import("@/pages/Home.vue"),
+		redirect: {
+			name: "ProductsListPage",
+		},
 		meta: {
 			RequireLogin: false,
 		},
 	},
-	
-]
+	{
+		path: "/products",
+		name: "ProductsListPage",
+		component: () => import("@/pages/Products.vue"),
+	},
+];
 
 const router = createRouter({
 	history: createWebHistory("/store"),
 	routes,
-})
+});
 
 router.beforeEach(async (to, from, next) => {
-	let isLoggedIn = session.isLoggedIn
+	let isLoggedIn = session.isLoggedIn;
 	try {
-		await userResource.promise
+		await userResource.promise;
 	} catch (error) {
-		isLoggedIn = false
+		isLoggedIn = false;
 	}
 
 	if (to.meta.RequireLogin && !isLoggedIn) {
 		// redirect to login
-		window.location.href = "/login?redirect-to=/store" + to.fullPath
+		window.location.href = "/login?redirect-to=/store" + to.fullPath;
 	}
-	next()
+	next();
+});
 
-})
-
-export default router
+export default router;
