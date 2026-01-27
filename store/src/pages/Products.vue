0c1a0b191398b7bf17669413d9ee603e3412794a
FawazALhafiz <fawaztwix111@gmail.com>
2026-01-27 16:56:26 +0200

    feat: restructure Products page template for improved layout

--- store/src/pages/Products.vue
+++ store/src/pages/Products.vue
@@ -1,9 +1,23 @@
 <template>
-  <h1 class="text-2xl font-bold text-gray-800">
-   ✨ Products 👌
-  </h1>
+  <div>
+    <h1 class="text-2xl font-bold text-gray-800">
+      ✨ Products 👌
+    </h1>
+    
+    <div v-if="productsResource.data">
+      <p v-for="product in productsResource.list.data" :key="product.name" class="text-gray-600">
+        {{ product.name }} - {{ product.price }} {{ product.currency }}
+      </p>
+    </div>
+  </div>
 </template>
 
 <script setup>
+import { createListResource } from 'frappe-ui';
+const productsResource = createListResource({
+  doctype: 'Product',
+  fields: ['name', 'preview_image', 'price', 'currency'],
+  auto: true,
+});
 
 </script>
