0a2951d173f83dda49b137d0d04c1b4798d9174d
FawazALhafiz <fawaztwix111@gmail.com>
2026-01-30 17:04:47 +0200

    feat: improve Products page layout and enhance product display

--- store/src/pages/Products.vue
+++ store/src/pages/Products.vue
@@ -3,15 +3,19 @@
     <h1 class="text-2xl font-bold text-gray-800">
       ✨ Products 👌
     </h1>
-    
-    <div v-if="productslist">
+    <hr>
+    <h2 class="my-3 text-lg font-semibold">Our Products</h2>
+
+    <div v-if="productslist" class="mt-4 grid grid-cols-4 gap-3">
       <!-- Products List -->
-      
-      <div v-for="product in productslist" :key="product.name" class="text-gray-600">
-        <img :src="product.preview_image" alt="Product Image" class="w-24 h-24"/>
-        <p>{{ product.name }}</p>
-        <span>Price: {{ formatCurrency(product.price, product.currency) }}</span>
-      </div>
+      <a v-for="product in productslist" :key="product.name" href="#"
+          class="shadow-md rounded-sm p-3 flex flex-col justify-between" >
+        <div>
+          <img :src="product.preview_image" alt="Product Image" class="max-w-[100%] aspect-square object-contain"/>
+          <p class="text-gray-800 text-base">{{ product.name }}</p>
+        </div>
+        <span class="font-bold text-gray-900">{{ formatCurrency(product.price, product.currency) }}</span>
+      </a>
 
     </div>
   </div>
