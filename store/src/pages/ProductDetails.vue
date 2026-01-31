99083be947837daf4b1c4dbe559337fff01f615c
FawazALhafiz <fawaztwix111@gmail.com>
2026-01-31 16:04:31 +0200

    feat: enhance ProductDetails layout with image thumbnails and preview functionality

--- store/src/pages/ProductDetails.vue
+++ store/src/pages/ProductDetails.vue
@@ -7,17 +7,25 @@
             </h1>
 
             <div class="flex flex-col w-fit gap-3">
-                <img v-for="image in productDoc.images" :key="image.image_url" :src="image.image_url" alt="Product Image Thumbnail" 
-                class="h-16 border-2 border-gray-800/25 rounded cursor-pointer hover:border-gray-800"
-                />
+                <div :class='[ 
+                        "p-1 border-2 rounded cursor-pointer border-gray-800/25 hover:border-gray-800",
+                        currenPreviewImageUrl === image.image_url ? "border-gray-800/90": "" 
+                    ]'
+                    v-for="image in productDoc.images" :key="image.image_url"
+                    @click="currenPreviewImageUrl = image.image_url"
+                >
+                    <img class="h-16" :src="image.image_url" alt="Product Image Thumbnail"/>
+                </div>
             </div>
+
+            <img :src="currenPreviewImageUrl" alt="Product Preview Image" class="w-full max-w-lg mt-4 rounded border-2 border-gray-800/25" />
         </div>
     </div>
 </div>
 </template>
 
 <script setup>
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { useRoute } from 'vue-router';
 import { createDocumentResource } from 'frappe-ui';
 
@@ -27,9 +35,16 @@ const productName = route.params.name;
 const productResource = createDocumentResource({
     doctype: 'Product',
     name: productName,
-    auto: true, 
+    auto: true,
+    onSuccess(doc) {
+        if (doc && doc.preview_image) {
+            currenPreviewImageUrl.value = doc.preview_image;
+        }
+    },
 });
 
 const productDoc = computed(() => productResource.doc || {});
+
+const currenPreviewImageUrl = ref('');
 
 </script>
