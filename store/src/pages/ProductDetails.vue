12f8f6c085e0f23948c2b7e85f4989cc4aadb9b9
FawazALhafiz <fawaztwix111@gmail.com>
2026-01-31 18:15:42 +0200

    feat(responsive desing): improve layout of ProductDetails by enhancing image carousel and preview display

--- store/src/pages/ProductDetails.vue
+++ store/src/pages/ProductDetails.vue
@@ -6,19 +6,24 @@
                 {{ productDoc.name }}
             </h1>
 
-            <div class="flex flex-col w-fit gap-3">
-                <div :class='[ 
-                        "p-1 border-2 rounded cursor-pointer border-gray-800/25 hover:border-gray-800",
-                        currenPreviewImageUrl === image.image_url ? "border-gray-800/90": "" 
-                    ]'
-                    v-for="image in productDoc.images" :key="image.image_url"
-                    @click="currenPreviewImageUrl = image.image_url"
-                >
-                    <img class="h-16" :src="image.image_url" alt="Product Image Thumbnail"/>
+            <div class="sm:flex sm:gap-2">
+                <!-- Image Carousel -->
+                <div class="flex flex-col w-fit gap-3">
+                    <div :class='[ 
+                            "p-1 border-2 rounded cursor-pointer border-gray-800/25 hover:border-gray-800",
+                            currenPreviewImageUrl === image.image_url ? "border-gray-800/90": "" 
+                        ]'
+                        v-for="image in productDoc.images" :key="image.image_url"
+                        @click="currenPreviewImageUrl = image.image_url"
+                    >
+                        <img class="h-16" :src="image.image_url" alt="Product Image Thumbnail"/>
+                    </div>
                 </div>
-            </div>
 
-            <img :src="currenPreviewImageUrl" alt="Product Preview Image" class="w-full max-w-lg mt-4 rounded border-2 border-gray-800/25" />
+                <!-- Current Image Preview -->
+                <img :src="currenPreviewImageUrl" alt="Product Preview Image" class="sm:max-w-lg rounded border-2 border-gray-800/25" />
+            </div>
+        
         </div>
     </div>
 </div>
