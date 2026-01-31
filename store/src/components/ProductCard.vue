f0bb397abc54274686ae95abd15bcec28c63aa0a
FawazALhafiz <fawaztwix111@gmail.com>
2026-01-31 08:44:42 +0200

    feat: update ProductCard to use router-link for navigation and link ProductDetails to it

--- store/src/components/ProductCard.vue
+++ store/src/components/ProductCard.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
 
-        <a href="#"
+        <router-link :to="{ name: 'ProductDetailsPage', params: { name: product.name } }"
              class="shadow-md rounded-sm p-3 flex flex-col justify-between" >
            <div>
              <img :src="product.preview_image" alt="Product Image" class="max-w-[100%] aspect-square object-contain"/>
              <p class="text-gray-800 text-base">{{ product.name }}</p>
            </div>
            <span class="font-bold text-gray-900">{{ formatCurrency(product.price, product.currency) }}</span>
-         </a>
+         </router-link>
 
     </div>
 </template>
--- store/src/pages/ProductDetails.vue
+++ store/src/pages/ProductDetails.vue
@@ -1,7 +1,14 @@
 <template>
-    <h1>Product Details</h1>
+    <div>
+        <h1>Product Details</h1>
+        <p>Product Name: {{ name }}</p>
+    </div>
 </template>
 
 <script setup>
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const { name } = route.params;
 
 </script>
